af30ad245ad3426e8c63aee88b188a3cd0bd2ff6
JD Rowley <jdrowley1989@gmail.com>
2021-12-01 14:31:49 -0700

    tested contact component

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('Contact component', () => {
+    it('renders', () => {
+        render(<ContactForm></ContactForm>);
+    });
+
+    it ('matches snapshot', () => {
+        const { asFragment } = render(<ContactForm />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it ('renders', () => {
+        const { getByTestId } = render(<ContactForm />);
+
+        expect(getByTestId('h1tag')).toHaveTextContent('Contact Me')
+    })
+})
